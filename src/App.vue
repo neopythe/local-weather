cb8d2b3bb648d72e75476c3a86e70899679c15dc
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-15 19:47:17 -0600

    fetch and render some data from the weather API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+import { useWeatherStore } from '@/stores/weather';
+
+const weatherStore = useWeatherStore();
+
+onMounted(() => weatherStore.fetchWeather('Osaka'));
+</script>
 
 <template>
-  <h1>Hello, World!</h1>
+  <div class="flex flex-col gap-4">
+    <div class="m-4">
+      <h2 class="text-lg font-semibold">
+        Weather in {{ weatherStore.weather?.name }}:
+      </h2>
+      <div class="p-2">
+        <p>{{ weatherStore.weather?.main.temp }} degrees</p>
+        <p class="font-light">
+          {{ weatherStore.weather?.weather[0].description }}
+        </p>
+      </div>
+    </div>
+    <p class="m-4 px-8">{{ weatherStore.weather }}</p>
+  </div>
 </template>
