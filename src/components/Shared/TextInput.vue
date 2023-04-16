c4631a1783f75465becc298d2eb9baa3d5b07a11
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-16 14:18:25 -0600

    create reusable component for text input

---
+++ src/components/Shared/TextInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" :value="modelValue" @input="handleInput" />
+</template>
+
+<script setup lang="ts">
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const handleInput = ($event: Event) => {
+  const target = $event.target as HTMLInputElement;
+  emit('update:modelValue', target.value);
+};
+</script>
+
+<style scoped>
+input {
+  @apply h-12 w-full rounded p-4 font-semibold text-blue-600 focus:outline-none;
+}
+</style>
