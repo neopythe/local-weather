f97006e2b09b32ec0394c750930dbd3e646a7aad
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-15 18:15:20 -0600

    specify type for return value of Axios get request

--- src/api/getWeather.ts
+++ src/api/getWeather.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+import type { Weather } from './types';
+
 const getWeather = async (city: string, units: string = 'metric') => {
-  const { data } = await axios.get(
+  const { data } = await axios.get<Weather>(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
       import.meta.env.VITE_APP_WEATHER_API_KEY
     }&units=${units}`
