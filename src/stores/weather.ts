897ccfa35ba5e68133dda54755fac426f72f3bc6
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-16 08:03:45 -0600

    set up isLoading boolean on weather store

--- src/stores/weather.ts
+++ src/stores/weather.ts
@@ -6,16 +6,19 @@ import getWeather from '@/api/getWeather';
 
 export interface WeatherState {
   weather: Weather | null;
+  isLoading: boolean;
 }
 
 export const useWeatherStore = defineStore('weather', {
   state: (): WeatherState => ({
     weather: null,
+    isLoading: true,
   }),
   actions: {
     async fetchWeather(city: string) {
       const weather = await getWeather(city);
       this.weather = weather;
+      this.isLoading = false;
     },
   },
 });
