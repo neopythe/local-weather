4b3eb4d614f67ff4003fbfa3bc7d365a3b7c7030
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-04-19 23:25:27 -0600

    create simple reusable button component

---
+++ src/components/Shared/ActionButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    :class="[
+      'bg-blue-500',
+      'hover:bg-blue-700',
+      'text-white',
+      'font-bold',
+      'py-2',
+      'px-4',
+      'rounded',
+      'focus:outline-none',
+      'focus:shadow-outline',
+      'transition',
+      'duration-500',
+      'ease-in-out',
+      'transform',
+      'hover:-translate-y-1',
+      'hover:scale-110',
+    ]"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: { type: String, required: true },
+});
+</script>
